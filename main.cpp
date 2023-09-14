5b668622cd49f9b2af2946b21d27325d533bd277
noumanalisyed <nouman.ali.syed@gmail.com>
2023-09-14 15:36:34 +0500

    Update Point class with Default Constructor, Overloaded Constructor, Print function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
 
+class Point {
+public :
+    Point(){
+        x = 0;
+        y = 0;
+    }
+    Point(int a,int b){
+        x = a;
+        y = b;
+    }
+    void print(){
+        cout<<"[ "<<x<<" , "<<y<<" ] "<<endl;
+    }
+
+private:
+    int x;
+    int y;
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Point point;
+    Point point1(10,12);
+    point.print();
+    point1.print();
     return 0;
 }

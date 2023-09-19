2bf244698d072de648ef8d6017ad66f4531503dc
noumanalisyed <nouman.ali.syed@gmail.com>
2023-09-19 16:18:04 +0500

    Adding getter and setter functions

--- main.cpp
+++ main.cpp
@@ -6,23 +6,42 @@ public :
     Point(){
         x = 0;
         y = 0;
+        cout<<"Point -- Default constructor called"<<endl;
     }
     Point(int a,int b){
-        x = a;
-        y = b;
+        x = (a >= 0 && a <= 100) ? a : 0;
+        y = (b >= 0 && b <= 100) ? b : 0; // ternary operator
+        cout<<"Point -- Overloaded constructor called"<<endl;
     }
     void print(){
         cout<<"[ "<<x<<" , "<<y<<" ] "<<endl;
     }
 
+    int getX(){
+        return x;
+    }
+    void setX(int a){
+        x = (a >= 0 && a <= 100) ? a : 0;
+    }
+    int getY(){
+        return y;
+    }
+    void setY(int a){
+        y = (a >= 0 && a <= 100) ? a : 0;
+    }
 private:
     int x;
     int y;
 };
 int main() {
     Point point;
-    Point point1(10,12);
+    Point point1(110,90);
     point.print();
     point1.print();
+   // point1.y = 990;
+    point1.print();
+    point.setX(199);
+    point.setY(210);
+    point.print();
     return 0;
 }
